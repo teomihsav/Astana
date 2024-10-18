8465e297696bc725a07a4c385f9c27cb66354dbc
teomihsav <teomihsav76@gmail.com>
2024-10-19 02:27:43 +0300

    align sub menu

--- astana/src/components/common/MenuLeftBtn/MenuTransformation/MenuTransformation.tsx
+++ astana/src/components/common/MenuLeftBtn/MenuTransformation/MenuTransformation.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const MenuTransformation = () => {
   return <div className="boxCont">
-    <div className="textUp">Profile</div>
+    {/* <div className="textUp">Profile</div> */}
+    <div className="textUp">Login</div>
     <div className="box">sub</div>
+    {/* <div className="box">
+      <div className='label'>username</div>
+      <input className='inputProfile' />
+      <div className='label'>password</div>
+      <input className='inputProfile' />
+    </div> */}
     <div className="textOne">TextOne</div>
     <div className="boxOne">sub one</div>
-    <div className="textTwo">TextTwo</div>
-    <div className="boxTwo">sub two</div>
+    <div className="textTwo">Contact</div>
+    <div className="boxTwo">contact@ben.bg</div>
   </div>
-  //   <div className="textUp">Login</div>
-  // <div className="box">
-  //   <div className='label'>username</div>
-  //   <input className='inputProfile' />
-  //   <div className='label'>password</div>
-  //   <input className='inputProfile' />
-  // </div>
 }
 
 export default MenuTransformation
